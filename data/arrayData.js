7fc61da5dbe9dd7ccc2e4042338668a274ab7ebd
harith.zainudin <harith.zainudin@ecloudvalley.com>
2022-05-14 21:41:12 +0800

    Add data folder to categorise data

---
+++ data/arrayData.js
@@ -0,0 +1,41 @@
+const myClassmates = ["John", "Jane", "Jack", "Jill", "Joe"];
+const classmatesInfo = [
+  {
+    name: "John",
+    age: 20,
+  },
+  {
+    name: "Jane",
+    age: 21,
+  },
+  {
+    name: "Jack",
+    age: 22,
+  },
+  {
+    name: "Jill",
+    age: 20,
+  },
+  {
+    name: "Joe",
+    age: 20,
+  },
+];
+
+const countries = [
+  "Nigeria",
+  "Ghana",
+  "Singapore",
+  "USA",
+  "Malaysia",
+  "Indonesia",
+  "Japan",
+  "Brunei",
+  "Korea",
+];
+
+module.exports = {
+  myClassmates,
+  countries,
+  classmatesInfo,
+};
